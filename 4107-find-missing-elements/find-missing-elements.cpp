c871624ef67bded16611be604a81006fff2032ca
GauravSingh7400 <153507869+GauravSingh7400@users.noreply.github.com>
2026-08-04 12:25:43 +0530

    Time: 3 ms (49.33%) | Memory: 33.7 MB (28.39%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,19 +1,48 @@
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         int mi = *min_element(nums.begin() , nums.end());
+//           int mx = *max_element(nums.begin() , nums.end());
+//           vector<bool> e(mx - mi + 1 , false);
+//           for(int num : nums){
+//             e[num - mi ] = true;
+//           }
+          
+//          vector<int> ans;
+
+      
+//         for (int i = 0; i < e.size(); i++) {
+//             if (!e[i]) {
+//                 ans.push_back(i + mi);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int mi = *min_element(nums.begin() , nums.end());
-          int mx = *max_element(nums.begin() , nums.end());
-          vector<bool> e(mx - mi + 1 , false);
-          for(int num : nums){
-            e[num - mi ] = true;
-          }
-          
-         vector<int> ans;
+        int minimum = nums[0];
+        int maximum = nums[0];
 
-      
-        for (int i = 0; i < e.size(); i++) {
-            if (!e[i]) {
-                ans.push_back(i + mi);
+        unordered_set<int> st;
+
+        for (int n : nums) {
+            st.insert(n);
+
+            if (n < minimum)
+                minimum = n;
+
+            if (n > maximum)
+                maximum = n;
+        }
+
+        vector<int> ans;
+
+        for (int i = minimum; i <= maximum; i++) {
+            if (st.count(i) == 0) {
+                ans.push_back(i);
             }
         }
 

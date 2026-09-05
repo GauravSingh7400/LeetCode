76cc983e927355662bbcb3a00c19f6e05f2a5003
GauravSingh7400 <153507869+GauravSingh7400@users.noreply.github.com>
2026-09-05 14:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (94.92%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ans = 0;
+        long long ones = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] == '1') {
+                ones++;
+            }
+            else {
+                ans += ones;
+            }
+        }
+
+        return ans;
+    }
+};

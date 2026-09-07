2082e49b28bc833b30472b45fbbe0c2759fe727f
GauravSingh7400 <153507869+GauravSingh7400@users.noreply.github.com>
2026-09-07 10:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (99.83%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n = nums.size();
+        int j = 1;
+        int i=0;
+        while(i < n && j < n){
+            if(nums[i] % 2 ==  0){
+            i+=2;
+
+            }else if(nums[j] % 2 != 0){
+                j+=2;
+            }else{
+                swap(nums[i] , nums[j]);
+                i+=2;
+                j+=2;
+
+            }
+        }
+        return nums;
+        
+    }
+};
